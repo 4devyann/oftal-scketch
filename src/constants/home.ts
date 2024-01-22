3c863e2892b1e240f6b48c6f91a546de4a8a74f9
jasterVenturus <yjasterdearaujo@apple.com>
2024-01-22 14:22:38 -0300

    (enhance) [home] leaving just on banner

--- src/constants/home.ts
+++ src/constants/home.ts
@@ -18,34 +18,34 @@ export const homeItems: HomeItemType[] = [
     invert: false,
     banner: true
   },
-  {
-    image: MedicineImage,
-    title: 'Inovações em Cirurgia a Laser para os Olhos: Um Amanhã Mais Nítido',
-    description: 'Adentre o mundo da cirurgia a laser para os olhos e veja como tecnologias inovadoras estão remodelando o cenário da correção visual. Descubra os benefícios, desmistifique mitos e faça uma escolha informada para uma vida sem óculos ou lentes de contato.',
-    invert: true
-  },
-  {
-    image: MedicineImage,
-    title: 'Saúde Ocular em Todas as Idades: Nutrindo a Visão da Infância à Terceira Idade"',
-    description: 'Seus olhos evoluem com o tempo, e sua atenção à saúde ocular também deve evoluir. Explore guias abrangentes sobre cuidados oculares pediátricos, desafios visuais na idade adulta e estratégias para manter uma saúde ocular ideal ao envelhecer. Capacite-se com conhecimento para uma vida de visão clara.',
-    invert: false
-  },
-  {
-    image: MedicineImage,
-    title: 'Defesa Contra a Luz Azul: Protegendo Seus Olhos na Era Digital',
-    description: 'Mergulhe na era digital com confiança ao entender o impacto da luz azul em seus olhos. Descubra conselhos de especialistas, medidas de proteção e óculos especializados projetados para proteger seus olhos da tensão causada pelo tempo prolongado diante de telas. Priorize o bem-estar ocular na era da tecnologia.',
-    invert: true
-  },
-  {
-    image: MedicineImage,
-    title: 'A Janela para Sua Saúde: Olhos como Indicadores do Bem-Estar Geral',
-    description: 'Seus olhos podem revelar mais do que apenas seu estado de visão. Explore as conexões intricadas entre a saúde ocular e o bem-estar geral. Saiba como os exames oftalmológicos podem servir como detectores precoces de doenças sistêmicas, enfatizando a importância de check-ups regulares para uma abordagem holística à saúde.',
-    invert: false
-  },
-  {
-    image: MedicineImage,
-    title: 'Empoderando Visionários: Histórias de Sucesso em Oftalmologia',
-    description: 'Inspire-se em jornadas reais de indivíduos que transformaram suas vidas por meio de intervenções oftalmológicas. Desde superar deficiências visuais até abraçar uma clareza recém-descoberta, essas histórias destacam o poder transformador da oftalmologia. Junte-se a uma comunidade de visionários e compartilhe sua própria história de sucesso.',
-    invert: true
-  },
+  // {
+  //   image: MedicineImage,
+  //   title: 'Inovações em Cirurgia a Laser para os Olhos: Um Amanhã Mais Nítido',
+  //   description: 'Adentre o mundo da cirurgia a laser para os olhos e veja como tecnologias inovadoras estão remodelando o cenário da correção visual. Descubra os benefícios, desmistifique mitos e faça uma escolha informada para uma vida sem óculos ou lentes de contato.',
+  //   invert: true
+  // },
+  // {
+  //   image: MedicineImage,
+  //   title: 'Saúde Ocular em Todas as Idades: Nutrindo a Visão da Infância à Terceira Idade"',
+  //   description: 'Seus olhos evoluem com o tempo, e sua atenção à saúde ocular também deve evoluir. Explore guias abrangentes sobre cuidados oculares pediátricos, desafios visuais na idade adulta e estratégias para manter uma saúde ocular ideal ao envelhecer. Capacite-se com conhecimento para uma vida de visão clara.',
+  //   invert: false
+  // },
+  // {
+  //   image: MedicineImage,
+  //   title: 'Defesa Contra a Luz Azul: Protegendo Seus Olhos na Era Digital',
+  //   description: 'Mergulhe na era digital com confiança ao entender o impacto da luz azul em seus olhos. Descubra conselhos de especialistas, medidas de proteção e óculos especializados projetados para proteger seus olhos da tensão causada pelo tempo prolongado diante de telas. Priorize o bem-estar ocular na era da tecnologia.',
+  //   invert: true
+  // },
+  // {
+  //   image: MedicineImage,
+  //   title: 'A Janela para Sua Saúde: Olhos como Indicadores do Bem-Estar Geral',
+  //   description: 'Seus olhos podem revelar mais do que apenas seu estado de visão. Explore as conexões intricadas entre a saúde ocular e o bem-estar geral. Saiba como os exames oftalmológicos podem servir como detectores precoces de doenças sistêmicas, enfatizando a importância de check-ups regulares para uma abordagem holística à saúde.',
+  //   invert: false
+  // },
+  // {
+  //   image: MedicineImage,
+  //   title: 'Empoderando Visionários: Histórias de Sucesso em Oftalmologia',
+  //   description: 'Inspire-se em jornadas reais de indivíduos que transformaram suas vidas por meio de intervenções oftalmológicas. Desde superar deficiências visuais até abraçar uma clareza recém-descoberta, essas histórias destacam o poder transformador da oftalmologia. Junte-se a uma comunidade de visionários e compartilhe sua própria história de sucesso.',
+  //   invert: true
+  // },
 ];
